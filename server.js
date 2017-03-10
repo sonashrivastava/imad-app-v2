09b7b1da3ed274c10d12b83041c9f607479d20d9
sonashrivastava <godfather.ag90@gmail.com>
2017-03-10 20:49:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articleone = {
+
+var articles = {
+    
+
+
+'article-one' : {
     title: 'Article-one | sonashrivastava',
     heading:'Article-one',
     date: '10 march 2017',
@@ -20,7 +25,37 @@ var articleone = {
         this is my article-one.
         my first article.
     </p>`
+},
+'article-two' : {
+    title: 'Article-two | sonashrivastava',
+    heading:'Article-two',
+    date: '15 march 2017',
+    content: ` <p>
+        this is my article-two.
+        my second article.
+
+    </p>` 
+},
+
+
+    
+'article-three' : {
+     title: 'Article-three | sonashrivastava',
+    heading:'Article-three',
+    date: '20 march 2017',
+    content: ` <p>
+        this is my article-three.
+        my third article.
+   
+    </p>`
+}
+    
 };
+
+    
+   
+
+
 function createTemplate(data) {
     var title=data.title;
     var  date=data.date;
@@ -62,16 +97,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res) {
-     res.send(createTemplate(articleone));
-  
+app.get('/:articleName', function(req,res) {
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two', function (req, res) {
-  res.send('article two will be requested and served here');
-});
-app.get('/article-three', function (req, res) {
-  res.send('article three will be requested and served here');
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
