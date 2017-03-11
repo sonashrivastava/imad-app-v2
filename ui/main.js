23be0fb5e640505157762cc4adb6cb3204e3fcce
sonashrivastava <godfather.ag90@gmail.com>
2017-03-11 12:14:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,17 @@ img.onclick = function()
 var button = document.getElementById('counter');
 var counter =0;
 button.onclick = function() {
-    counter= counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE)
+        { 
+            if(request.status === 200)
+        { var counter = request.responseText;
+        
+        }
+    }
+};
+request.open('GET' , 'http://sonashrivastava.imad.hasura-app.io/counter', true);
+request.send(null);
 };
