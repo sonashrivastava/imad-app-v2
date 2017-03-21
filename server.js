eda84f9ae36ebec5feb48da5541d80a1f1d21edb
sonashrivastava <godfather.ag90@gmail.com>
2017-03-21 23:44:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     database: 'sonashrivastava',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password:process.env.DB_PASSWORD
+    password:db-sonashrivastava-49490
 };
 
 var app = express();
